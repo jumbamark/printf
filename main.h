3fc06cbfafb29ca1478b3acd70a62d2f5dc6d7e4
jumbamark <jumbamark@yahoo.com>
2022-04-19 10:56:21 -0700

    Modified header file

--- main.h
+++ main.h
@@ -24,6 +24,6 @@ int print_str(va_list str_list);
 int print_char(va_list char_list);
 int percentage(__attribute__((unused))va_list param);
 int (*converter(char s))(va_list);
-int print_digit(va_list intlist);
+int print_digit(va_list int_list);
 
 #endif /* MAIN_H */
