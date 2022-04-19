69d0c6ba444cc71814d15aa445965c82e923c54a
jumbamark <jumbamark@yahoo.com>
2022-04-19 07:50:14 -0700

    Added header files

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
