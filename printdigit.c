5bafc0687a5d64bbc23b6398a996807c7d33305f
lynnemunini <muninilynne65@gmail.com>
2022-04-19 10:40:36 -0700

    Modified printdigit

--- printdigit.c
+++ printdigit.c
@@ -3,41 +3,41 @@
 
 /**
  * print_digit - function to get int variable
- * @intlist: variable list
+ * @int_list: variable list
  *
  * Return: integer
  */
 
-int print_digit(va_list intlist)
+int print_digit(va_list int_list)
 {
-	int i, div, len;
+	int n, division, length;
 	unsigned int num;
 
-	i = va_arg(intlist, int);
-	div = 1;
-	len = 0;
+	i = va_arg(int_list, int);
+	division = 1;
+	length = 0;
 
-	if (i < 0)
+	if (n < 0)
 	{
-		len += _putchar('-');
-		num = i * -1;
+		length += _putchar('-');
+		num = n * -1;
 	}
 	else
 	{
-		num = i;
+		num = n;
 	}
 
-	for (; num / div > 9;)
+	for (; num / division > 9;)
 	{
-		div *= 10;
+		division *= 10;
 	}
 
-	for (; div != 0;)
+	for (; division != 0;)
 	{
-		len += _putchar('0' + (num / div));
-		num %= div;
-		div /= 10;
+		length += _putchar('0' + (num / division));
+		num %= division;
+		division /= 10;
 	}
 
-	return (len);
+	return (length);
 }
