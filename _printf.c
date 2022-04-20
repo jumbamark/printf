c23112524ae4e5aea1e375fa2680b8301adcfa08
jumbamark <jumbamark@yahoo.com>
2022-04-20 12:24:55 -0700

    Update file

--- _printf.c
+++ _printf.c
@@ -12,31 +12,31 @@
 int _printf(const char *format, ...)
 {
 	va_list arguments;
-	int sum = 0;
-	int n = 0;
+	unsigned int sum = 0;
+	unsigned int n = 0;
 
-	va_start(arguments, format);
 	if (format == NULL)
 	{
 		return (-1);
 	}
-	else
+
+	va_start(arguments, format);
+	for (; format[n] != '\0'; n++)
 	{
-		for (; format[n] != '\0'; n++)
+		if (format[n] == '\0' || (format[n] == '%' && !format[n + 1]))
 		{
-			if (format[n] == '\0')
-			{
-				break;
-			}
-			else if (format[n] == '%' && format[n + 1])
-			{
-				sum += (*converter(format[n + 1]))(arguments);
-				n++;
-			}
-			else
-			{
-				sum += _putchar(format[n]);
-			}
+			return (-1);
+		}
+		else if (format[n] == '%' && (format[n + 1] == 'd' ||
+			format[n + 1] == 'i' || format[n + 1] == 's' ||
+			format[n + 1] == 'c' || format[n + 1] == '%'))
+		{
+			sum += (*converter(format[n + 1]))(arguments);
+			n++;
+		}
+		else
+		{
+			sum += _putchar(format[n]);
 		}
 	}
 	va_end(arguments);
